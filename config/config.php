a121b404fa43c6f6b907f9f301f6c6affbda1c9c
Sarunporn Phiewngam <06604@sukhon.ac.th>
2026-03-24 13:26:11 +0700

    testupload

--- config/config.php
+++ config/config.php
@@ -9,7 +9,7 @@ define('APP_ENV', 'local'); // local | production
 
 // ตั้งค่า DB (แก้ให้ตรงกับเครื่องของคุณ)
 define('DB_DRIVER', 'mysql');  // mysql | sqlite | pgsql
-define('DB_HOST', '127.0.0.1');
+define('DB_HOST', '1211');
 define('DB_PORT', '3306');
 define('DB_NAME', 'school_app');
 define('DB_USER', 'root');
