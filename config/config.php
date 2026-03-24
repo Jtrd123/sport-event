2de08e2cfd6b05414dbcf1f9642700e1db8367d5
Sarunporn Phiewngam <06604@sukhon.ac.th>
2026-03-24 14:07:53 +0700

    updateconfig

--- config/config.php
+++ config/config.php
@@ -3,13 +3,13 @@
 define('APP_NAME', 'SPORT-EVENT');
 
 // *** แก้ path ให้ตรงโปรเจกต์ของคุณบน XAMPP ***
-define('BASE_URL', 'sport-event/public'); // << สำคัญ: มีสแลชนำหน้า และไม่มีสแลชท้าย
+define('BASE_URL', ''); // << สำคัญ: มีสแลชนำหน้า และไม่มีสแลชท้าย
 
 define('APP_ENV', 'local'); // local | production
 
 // ตั้งค่า DB (แก้ให้ตรงกับเครื่องของคุณ)
 define('DB_DRIVER', 'mysql');  // mysql | sqlite | pgsql
-define('DB_HOST', '172.17.0.3');
+define('DB_HOST', 'mysql_server');
 define('DB_PORT', '3306');
 define('DB_NAME', 'school_app');
 define('DB_USER', 'root');
